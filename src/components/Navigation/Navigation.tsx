6295d5280a9fe9b26f6bcba5a4702b9213ff3c0a
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-05-24 15:03:29 -0700

    Update link name.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -12,7 +12,7 @@ function Navigation()
 			<div className="logo"><A href="/">CKB<small>.tools</small></A></div>
 			<ul className="menu1">
 				<li><A href="/" className={(currentPath==='/')?'active':''} title="Home"><i className="fas fa-home"></i></A></li>
-				<li><A href="/sudt" className={(currentPath==='/sudt')?'active':''} title="SUDT">SUDT</A></li>
+				<li><A href="/sudt" className={(currentPath==='/sudt')?'active':''} title="SUDT Tool">SUDT Tool</A></li>
 			</ul>
 			<ul className="menu2">
 				<li><A href="/about" className={(currentPath==='/about')?'active':''} title="About"><i className="fas fa-info-circle"></i></A></li>
