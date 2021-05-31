d5a7faed3a70478165c43d13720c78f95658ee09
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-05-31 14:27:14 -0700

    Add section descriptions and special address notes.

--- src/containers/Address/Address.tsx
+++ src/containers/Address/Address.tsx
@@ -7,6 +7,12 @@ import NullCollector from '../../collectors/NullCollector';
 import NullProvider from '../../providers/NullProvider';
 import './Address.scss';
 
+const KNOWN_ADDRESSES: {[key: string]: any} =
+{
+	'ckt1qyqvsv5240xeh85wvnau2eky8pwrhh4jr8ts8vyj37': <>Note: This address is genesis issued cell #1 on a CKB dev chain. More information can be found <a href="https://docs.nervos.org/docs/basics/guides/devchain" target="_blank" rel="noreferrer">here.</a></>,
+	'ckt1qyqywrwdchjyqeysjegpzw38fvandtktdhrs0zaxl4': <>Note: This address is genesis issued cell #2 on a CKB dev chain. More information can be found <a href="https://docs.nervos.org/docs/basics/guides/devchain" target="_blank" rel="noreferrer">here.</a></>,
+};
+
 enum AddressComponents
 {
 	CodeHash,
@@ -25,7 +31,7 @@ enum AddressFlags
 	Testnet,
 }
 
-async function initPwCore(chain: "mainnet"|"testnet")
+async function initPwCore(chain: 'mainnet'|'testnet')
 {
 	const provider = new NullProvider();
 	const collector = new NullCollector();
@@ -43,6 +49,27 @@ function Flag(props: {checked: boolean})
 		src = 'checkbox-unchecked.png';
 	const altText = (props.checked) ? 'Checked Checkbox' : 'Unchecked Checkbox';
 	const html = <img className="checkbox" src={src} alt={altText} />;
+
+	return html;
+}
+
+function generateNotesSection(valid: boolean, inputAddress: Address|null)
+{
+	if(!valid || !inputAddress)
+		return null;
+
+	const address = inputAddress.toCKBAddress();
+
+	let html = null;
+	if(_.has(KNOWN_ADDRESSES, address))
+	{
+		html =
+		(
+			<section className="notes">
+				{KNOWN_ADDRESSES[address]}
+			</section>
+		);
+	}
 
 	return html;
 }
@@ -152,43 +179,56 @@ function Component()
 			</p>
 			<form id="address-form" onSubmit={()=>false}>
 				<section>
-					<label>
+					<label title='A Nervos CKB address normally started with either "ckb" or "ckt".'>
 						Nervos CKB Address
-						<input id="input-address" className={(valid)?'valid':'invalid'} type="text" onChange={handleInputAddressChange} ref={inputAddressRef} placeholder="Enter a Nervos CKB address to get started." />
+						<input id="input-address" className={(valid)?'valid':'invalid'} type="text" onChange={handleInputAddressChange} ref={inputAddressRef} placeholder="Enter a Nervos CKB address to get started. eg: ckt1qyqvsv5240xeh85wvnau2eky8pwrhh4jr8ts8vyj37" />
 					</label>
 				</section>
 				<section>
-					<label>
+					<label title="A script code hash indicates the cell dep required for execution.">
 						Lock Script Code Hash
 						<input type="text" readOnly={true} value={getAddressComponent(AddressComponents.CodeHash)} />
 					</label>
-					<label>
+					<label title="A script hash type indicates how the code hash should be matched against a cell dep.">
 						Lock Script Hash Type
 						<input type="text" readOnly={true} value={getAddressComponent(AddressComponents.HashType)} />
 					</label>
-					<label>
+					<label title="The script args are passed to the script code during execution.">
 						Lock Script Args
 						<input type="text" readOnly={true} value={getAddressComponent(AddressComponents.Args)} />
 					</label>
 				</section>
 				<section>
-					<label>
+					<label title="A script hash is a 256-bit Blake2b hash of the script structure once it has been serialized in the Molecule format.">
 						Lock Script Hash
 						<input type="text" readOnly={true} value={getAddressComponent(AddressComponents.LockHash)} />
 					</label>
 				</section>
 				<section className="flags">
 					<fieldset>
-						<label><span className="label">Mainnet</span> <Flag checked={getAddressFlag(AddressFlags.Mainnet)} /></label>
-						<label><span className="label">Testnet</span> <Flag checked={getAddressFlag(AddressFlags.Testnet)} /></label>
+						<label title='A Mainnet address always starts with "ckb".'>
+							<span className="label">Mainnet</span> <Flag checked={getAddressFlag(AddressFlags.Mainnet)} />
+						</label>
+						<label title='A Testnet address always starts with "ckt".'>
+							<span className="label">Testnet</span> <Flag checked={getAddressFlag(AddressFlags.Testnet)} />
+						</label>
 					</fieldset>
 					<fieldset>
-						<label><span className="label">ACP</span> <Flag checked={getAddressFlag(AddressFlags.Acp)} /></label>
-						<label><span className="label">Default Lock</span> <Flag checked={getAddressFlag(AddressFlags.DefaultLock)} /></label>
-						<label><span className="label">Multi-Sig Lock</span> <Flag checked={getAddressFlag(AddressFlags.MultiSigLock)} /></label>
-						<label><span className="label">PW-Lock</span> <Flag checked={getAddressFlag(AddressFlags.PwLock)} /></label>
+						<label title="An Anyone Can Pay (ACP) lock allows a user to receive funds without creating a new cell.">
+							<span className="label">ACP</span> <Flag checked={getAddressFlag(AddressFlags.Acp)} />
+						</label>
+						<label title="The Default lock is also known as the SECP256k1-Blake160-Sighash lock.">
+							<span className="label">Default Lock</span> <Flag checked={getAddressFlag(AddressFlags.DefaultLock)} />
+						</label>
+						<label title="The Multi-Sig lock is also known as the SECP256k1-Blake160-MultiSig lock.">
+							<span className="label">Multi-Sig Lock</span> <Flag checked={getAddressFlag(AddressFlags.MultiSigLock)} />
+						</label>
+						<label title="The PW-Lock is used for compatibility with wallets from other chains, such as MetaMask.">
+							<span className="label">PW-Lock</span> <Flag checked={getAddressFlag(AddressFlags.PwLock)} />
+						</label>
 					</fieldset>
 				</section>
+				{generateNotesSection(valid, inputAddress)}
 			</form>
 		</main>
 	);
