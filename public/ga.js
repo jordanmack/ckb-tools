fe53f6d1462f31b41c734d936404e0feb9a9cb8d
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-06-04 15:02:53 -0700

    Add Google Analytics tracking.

---
+++ public/ga.js
@@ -0,0 +1,7 @@
+if(window.location.host.startsWith("ckb.tools"))
+{
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){window.dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'G-YM8JGNY3H1');
+}
