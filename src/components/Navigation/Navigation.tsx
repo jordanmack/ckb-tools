621c980f2111372f6a8805a2a1bf6d0242ecbec2
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-07-03 17:28:09 -0700

    Add hamburger menu for small screen devices.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {A, usePath} from 'hookrouter';
 import './Navigation.scss';
 
 function Component()
 {
 	const currentPath = usePath();
+	const [isMenuActive, setMenuActive] = useState(false);
+
+	const toggleMenu = (e: React.SyntheticEvent) =>
+	{
+		// Match the backdrop height to the visible content container.
+		const main = document.querySelector('section.main') as HTMLElement;
+		const backdrop = document.querySelector('div.menu3-backdrop') as HTMLElement;
+		const contentHeight = Math.max(main.offsetHeight, window.innerHeight);
+		backdrop.style.height = contentHeight + 'px';
+
+		// Toggle the menu active state.
+		setMenuActive(!isMenuActive);
+	}
 
 	const html =
 	(
 		<nav className="primary">
+			<div className="background">&nbsp;</div>
 			<div className="logo"><A href="/">CKB<small>.tools</small></A></div>
 			<ul className="menu1">
 				<li><A href="/" className={(currentPath==='/')?'active':''} title="Home"><i className="fas fa-home"></i></A></li>
@@ -20,6 +34,17 @@ function Component()
 				<li><A href="/about" className={(currentPath==='/about')?'active':''} title="About"><i className="fas fa-info-circle"></i></A></li>
 				<li><A href="/contact" className={(currentPath==='/contact')?'active':''} title="Contact"><i className="far fa-envelope"></i></A></li>
 				<li><a href="https://github.com/jordanmack/ckb-tools" target="_blank" rel="noreferrer" title="GitHub"><i className="fab fa-github"></i></a></li>
+			</ul>
+			<button className="hamburger" onClick={toggleMenu}><i className="fas fa-bars"></i></button>
+			<div className={"menu3-backdrop"+((isMenuActive)?' active':'')} onClick={toggleMenu}>&nbsp;</div>
+			<ul className={"menu3"+((isMenuActive)?' active':'')}>
+				<li><A href="/" className={(currentPath==='/')?'active':''} title="Home" onClick={toggleMenu}>Home</A></li>
+				<li><A href="/address" className={(currentPath==='/address')?'active':''} title="Address Tool" onClick={toggleMenu}>Address</A></li>
+				<li><A href="/bootstrap" className={(currentPath==='/bootstrap')?'active':''} title="Bootstrap a Testnet Node" onClick={toggleMenu}>Bootstrap</A></li>
+				<li><A href="/sudt" className={(currentPath==='/sudt')?'active':''} title="SUDT Tool" onClick={toggleMenu}>SUDT</A></li>
+				<li><A href="/about" className={(currentPath==='/about')?'active':''} title="About" onClick={toggleMenu}>About</A></li>
+				<li><A href="/contact" className={(currentPath==='/contact')?'active':''} title="Contact" onClick={toggleMenu}>Contact</A></li>
+				<li><a href="https://github.com/jordanmack/ckb-tools" target="_blank" rel="noreferrer" title="GitHub" onClick={toggleMenu}>GitHub</a></li>
 			</ul>
 		</nav>
 	);
