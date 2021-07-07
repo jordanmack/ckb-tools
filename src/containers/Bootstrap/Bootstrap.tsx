80867a6a8dc0fd2663b40efbff441f9e6548ce74
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-07-07 16:00:28 -0700

    Update Bootstrap instructions.

--- src/containers/Bootstrap/Bootstrap.tsx
+++ src/containers/Bootstrap/Bootstrap.tsx
@@ -8,13 +8,13 @@ function Component()
 		<main className="bootstrap">
 			<img className="logo" src="./nervos-white.png" alt="Nervos Logo" />
 			<p>
-				After a new Nervos CKB node or CKB Indexer is setup, it takes several hours for the data to download and synchronize with the rest of the network.
+				It takes several hours for a new CKB node to download and synchronize with the rest of the network.
 				Fully synchronizing from the genesis block and verifying every transaction in the blockchain is the safest way.
 				This process is slow, but a necessary process for a Mainnet node.
 			</p>
 			<p>
 				However, this same level of security may not be required for a Testnet node since we are not dealing with real funds.
-				For convinience purposes, we provide recent snapshots of the Testnet chain data for the CKB node and CKB Indexer.
+				For convinience purposes, we provide recent snapshots of the Testnet chain data for the <a href="https://github.com/nervosnetwork/ckb/releases" target="_blank" rel="noreferrer">CKB Node</a>, <a href="https://github.com/nervosnetwork/ckb-indexer/releases" target="_blank" rel="noreferrer">CKB Indexer</a>, and <a href="https://github.com/nervosnetwork/ckb-cli/releases" target="_blank" rel="noreferrer">CKB-CLI</a>.
 				Downloading and using this chain data can save many hours when bootstrapping a new instance of a CKB node or a CKB Indexer.
 			</p>
 			<p>
@@ -38,6 +38,8 @@ function Component()
 			<ul>
 				<li>CKB Node: &lt;app_folder&gt;/data/db</li>
 				<li>CKB Indexer: indexer_data (path specified during setup)</li>
+				<li>CKB-CLI (Linux/MacOS): ~/.ckb-cli/index-v1/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
+				<li>CKB-CLI (Win 10): C:\&lt;USERNAME&gt;\.ckb-cli\index-v1\0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
 				<li>Neuron Wallet (Win 10): C:\Users\&lt;username&gt;\AppData\Roaming\Neuron\indexer_data\<wbr />0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
 			</ul>
 		</main>
