1421fe9687ced7d57d34b24abd82bf605735b1ea
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-08-03 15:04:47 -0700

    Add Lumos Indexer to Bootstrap description.

--- src/containers/Bootstrap/Bootstrap.tsx
+++ src/containers/Bootstrap/Bootstrap.tsx
@@ -14,7 +14,7 @@ function Component()
 			</p>
 			<p>
 				However, this same level of security may not be required for a Testnet node since we are not dealing with real funds.
-				For convinience purposes, we provide recent snapshots of the Testnet chain data for the <a href="https://github.com/nervosnetwork/ckb/releases" target="_blank" rel="noreferrer">CKB Node</a>, <a href="https://github.com/nervosnetwork/ckb-indexer/releases" target="_blank" rel="noreferrer">CKB Indexer</a>, and <a href="https://github.com/nervosnetwork/ckb-cli/releases" target="_blank" rel="noreferrer">CKB-CLI</a>.
+				For convinience purposes, we provide recent snapshots of the Testnet chain data for the <a href="https://github.com/nervosnetwork/ckb/releases" target="_blank" rel="noreferrer">CKB Node</a>, <a href="https://github.com/nervosnetwork/ckb-indexer/releases" target="_blank" rel="noreferrer">CKB Indexer</a>, <a href="https://github.com/nervosnetwork/lumos/tree/develop/packages/indexer" target="_blank" rel="noreferrer">Lumos Indexer</a>, and <a href="https://github.com/nervosnetwork/ckb-cli/releases" target="_blank" rel="noreferrer">CKB-CLI</a>.
 				Downloading and using this chain data can save many hours when bootstrapping a new instance of a CKB node or a CKB Indexer.
 			</p>
 			<p>
@@ -33,13 +33,14 @@ function Component()
 				<li>Restart the app and allow it to complete the syncing process.</li>
 			</ol>
 			<p>
-				Data folders for common applications: 
+				Default data folders for common applications: 
 			</p>
 			<ul>
 				<li>CKB Node: &lt;app_folder&gt;/data/db</li>
 				<li>CKB Indexer: indexer_data (path specified during setup)</li>
 				<li>CKB-CLI (Linux/MacOS): ~/.ckb-cli/index-v1/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
 				<li>CKB-CLI (Win 10): C:\&lt;USERNAME&gt;\.ckb-cli\index-v1\0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
+				<li>Lumos Indexer: &lt;project_folder&gt;/indexerdata</li>
 				<li>Neuron Wallet (Win 10): C:\Users\&lt;username&gt;\AppData\Roaming\Neuron\indexer_data\<wbr />0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
 			</ul>
 		</main>
