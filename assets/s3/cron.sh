77a71d706d74b9cdf42ee9f7bc8d015b0f04ae3f
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-08-23 15:16:42 -0700

    Remove log files before compression.

--- assets/s3/cron.sh
+++ assets/s3/cron.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Stopping Nervos services..."
-sudo systemctl stop nervos-testnet
+sudo systemctl stop nervos-ckb-cli-testnet
 sudo systemctl stop nervos-ckb-indexer-testnet
 sudo systemctl stop nervos-lumos-indexer-testnet
-sudo systemctl stop nervos-ckb-cli-testnet
+sudo systemctl stop nervos-testnet
 sleep 10
 
 ckb_node_version=$(/home/nervos/ckb/ckb -V)
 ckb_node_version=${ckb_node_version/ckb /}
 filename1=/tmp/$(date +%Y%m%d)-nervos-ckb-node-${ckb_node_version}-testnet-snapshot.7z
 echo "Compressing CKB Node Testnet data to: ${filename1}..."
+rm /home/nervos/ckbt/data/db/LOG.old.*
 nice -n 19 7z a -r -mmt=1 -mx9 $filename1 /home/nervos/ckbt/data/db/*
 sleep 1
 
@@ -18,6 +19,7 @@ ckb_indexer_version=$(/home/nervos/ckb-indexer/ckb-indexer -V)
 ckb_indexer_version=${ckb_indexer_version/ckb-indexer /}
 filename2=/tmp/$(date +%Y%m%d)-nervos-ckb-indexer-${ckb_indexer_version}-testnet-snapshot.7z
 echo "Compressing CKB Indexer Testnet data to: ${filename2}..."
+rm /home/nervos/ckb-indexer/testnet-data/LOG.old.*
 nice -n 19 7z a -r -mmt=1 -mx9 $filename2 /home/nervos/ckb-indexer/testnet-data/*
 sleep 1
 
@@ -25,20 +27,22 @@ ckb_cli_version=$(/home/nervos/ckb/ckb-cli -V)
 ckb_cli_version=${ckb_cli_version/ckb-cli /}
 filename3=/tmp/$(date +%Y%m%d)-nervos-ckb-cli-${ckb_cli_version}-testnet-snapshot.7z
 echo "Compressing CKB-CLI Testnet data to: ${filename3}..."
+rm /home/nervos/.ckb-cli/index-v1/0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606/LOG.old.*
 nice -n 19 7z a -r -mmt=1 -mx9 $filename3 /home/nervos/.ckb-cli/index-v1/*
 sleep 1
 
 lumos_indexer_version=$(/home/jmack/.nvm/versions/node/v14.17.4/bin/node -p "require('/home/nervos/lumos-indexer/package.json').version")
 filename4=/tmp/$(date +%Y%m%d)-nervos-lumos-indexer-${lumos_indexer_version}-testnet-snapshot.7z
 echo "Compressing Lumos Indexer Testnet data to: ${filename4}..."
+rm /home/nervos/lumos-indexer/testnet-data/LOG.old.*
 nice -n 19 7z a -r -mmt=1 -mx9 $filename4 /home/nervos/lumos-indexer/testnet-data/*
 sleep 1
 
 echo "Starting Nervos services..."
 sudo systemctl start nervos-testnet
+sudo systemctl start nervos-ckb-cli-testnet
 sudo systemctl start nervos-ckb-indexer-testnet
 sudo systemctl start nervos-lumos-indexer-testnet
-sudo systemctl start nervos-ckb-cli-testnet
 sleep 1
 
 echo "Uploading archives to Amazon S3..."
