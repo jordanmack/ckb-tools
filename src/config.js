d8600dc66588d6e7f0df7e42f340f2ac3535b9aa
Jordan Mack <jordan.mack@brilliantnotion.com>
2023-01-09 10:32:10 -0800

    Update node endpoints.

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const config =
 {
 	testnet:
 	{
-		ckbRpcUrl: '//rpc-testnet.ckb.tools',
-		ckbIndexerUrl: '//indexer-testnet.ckb.tools',
-		ckbExplorerUrl: '//explorer.nervos.org/aggron/',
+		ckbRpcUrl: 'https://testnet.ckb.dev/ckb',
+		ckbIndexerUrl: 'https://testnet.ckb.dev/indexer',
+		ckbExplorerUrl: 'https://pudge.explorer.nervos.org/',
 		faucetUrl: 'https://faucet.nervos.org/'
 	},
 
 	mainnet:
 	{
-		ckbRpcUrl: '//rpc.ckb.tools',
-		ckbIndexerUrl: '//indexer.ckb.tools',
-		ckbExplorerUrl: '//explorer.nervos.org/'
+		ckbRpcUrl: 'https://mainnet.ckb.dev/ckb',
+		ckbIndexerUrl: 'https://mainnet.ckb.dev/indexer',
+		ckbExplorerUrl: 'https://explorer.nervos.org/'
 	},
 
 	sudtTransactionMonitorDelay: 2_000,				// Delay in milliseconds that the SUDT transaction monitor waits before checking if anything needs updating. 
