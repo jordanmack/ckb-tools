9ac8bce64d968cdb5221804b32164ecc4670b405
Jordan Mack <jordan.mack@brilliantnotion.com>
2023-05-01 10:16:06 -0700

    Update Bootstrap description.

--- src/containers/Bootstrap/Bootstrap.tsx
+++ src/containers/Bootstrap/Bootstrap.tsx
@@ -15,8 +15,8 @@ function Component()
 			</p>
 			<p>
 				However, this same level of security may not be required for a Testnet node since we are not dealing with real funds.
-				For convinience purposes, we provide recent snapshots of the Testnet chain data for the <a href="https://github.com/nervosnetwork/ckb/releases" target="_blank" rel="noreferrer">CKB Node</a>, and <a href="https://github.com/nervosnetwork/ckb-indexer/releases" target="_blank" rel="noreferrer">CKB Indexer</a>.
-				Downloading and using this chain data can save many hours when bootstrapping a new instance of a CKB node or a CKB Indexer.
+				For convinience purposes, we provide recent snapshots of the Testnet chain data for the <a href="https://github.com/nervosnetwork/ckb/releases" target="_blank" rel="noreferrer">CKB Node</a>.
+				Downloading and using this chain data can save many hours when bootstrapping a new instance of a CKB node.
 			</p>
 			<p>
 				You can view the most recent snapshots <a href="https://cdn-ckb-tools.sfo3.digitaloceanspaces.com/snapshots/snapshots.html?prefix=snapshots/" target="_blank" rel="noreferrer">here</a>.
@@ -25,22 +25,17 @@ function Component()
 				Basic Usage Instructions:
 			</p>
 			<ol>
-				<li>Install the software for the CKB node, CKB Indexer, or any application that uses them.</li>
+				<li>Install the software for the CKB node.</li>
 				<li>Follow the normal instructions to configure the software to use the Testnet.</li>
 				<li>On first launch, the software will create the required data folders. As soon as it begins the sync process, shut down the software.</li>
 				<li>Download a snapshot archive to your local hard drive.</li>
 				<li>Decompress the archive to a temporary folder using <a href="https://www.7-zip.org/" target="_blank" rel="noreferrer">7-Zip</a>.</li>
-				<li>Backup or delete the existing chain/indexer files from the application's data folder, the replace them with the contents of the archive.</li>
+				<li>Backup or delete the existing chain files from the application's data folder, the replace them with the contents of the archive.</li>
 				<li>Restart the app and allow it to complete the syncing process.</li>
 			</ol>
 			<p>
-				Default data folders for common applications: 
+				Default data folders for the CKB Node: &lt;app_folder&gt;/data/db
 			</p>
-			<ul>
-				<li>CKB Node: &lt;app_folder&gt;/data/db</li>
-				<li>CKB Indexer: indexer_data (path specified during setup)</li>
-				<li>Neuron Wallet (Win 10): C:\Users\&lt;username&gt;\AppData\Roaming\Neuron\indexer_data\<wbr />0x10639e0895502b5688a6be8cf69460d76541bfa4821629d86d62ba0aae3f9606</li>
-			</ul>
 		</main>
 	);
 
